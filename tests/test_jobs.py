14976f01c96759d58f243e0342f7abf987e14d94
Victoria Cadogan <vicasmail@gmail.com>
2021-02-26 09:26:24 -0500

    added comments to test workflow

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -4,6 +4,7 @@ import sqlite3
 
 
 def test_num_entries():
+    # test to check if we get at least 1000 entries
     url = f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3" \
           f"&api_key={secrets.api_key}&per_page=1"
     result = main.total_pages(url)
@@ -11,6 +12,7 @@ def test_num_entries():
 
 
 def test_funcs(db1='test.sqlite', db2='college_data.sqlite'):
+    # test the functions from main such as creating tables and adding data to it
     conn1, cur1 = main.create_connection(db1)
     conn2, cur2 = main.create_connection(db2)
     main.create_college_table(cur1)
@@ -22,6 +24,7 @@ def test_funcs(db1='test.sqlite', db2='college_data.sqlite'):
 
 
 def test_database():
+    # test to compare first 5 rows of each table and see if total number of entries match
     main.main()
     sql1 = """SELECT * FROM college_data LIMIT 5;"""
     sql2 = """SELECT * FROM jobs_data LIMIT 5"""
@@ -34,6 +37,7 @@ def test_database():
 
 
 def row_comparison(db1, db2, query, table_name):
+    # created a separate function that compares rows to avoid repetition/reuse for 2 tables
     conn1, cur1 = main.create_connection(db1)
     conn2, cur2 = main.create_connection(db2)
     res1 = cur1.execute(query)
@@ -53,19 +57,20 @@ def row_comparison(db1, db2, query, table_name):
             else:
                 print("!!!!!!!!PROBLEM " + db2 + " is missing Table:" + str(row2[0]))
                 assert False
-            exit()
     print("...Completed table comparison for " + table_name + ": all tables match")
     main.close_db(conn1)
     main.close_db(conn2)
 
 
 def entry_num(db1, db2, table_name):
+    # to avoid repetition/reuse for 2 tables, this function will take the output from another function and compare
     result1 = exec_sql(db1, table_name)
     result2 = exec_sql(db2, table_name)
     assert result1 == result2
 
 
 def exec_sql(filename, table_name):
+    # this function will return the number of entries from a table
     conn, cursor = main.create_connection(filename)
     cursor.execute("SELECT COUNT(*) FROM " + table_name)
     query = cursor.fetchone()[0]
@@ -74,6 +79,7 @@ def exec_sql(filename, table_name):
 
 
 def test_num_states():
+    # test to see if we get entries from all 50 states at least (in my case i also got data from territories)
     conn = sqlite3.connect('test.sqlite')
     cursor = conn.cursor()
     cursor.execute("SELECT COUNT(DISTINCT area_title) FROM jobs_data;")
